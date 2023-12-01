352c54c8b672bd67f182f48c7aedbe863b3a256c
Mary <mashadrozd1234@gmail.com>
2023-12-01 16:37:06 +0300

    fixed some bugs

--- Drozdova_2023/blog/views.py
+++ Drozdova_2023/blog/views.py
@@ -3,18 +3,44 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from .models import Post, Comment
+from .forms import CommentForm
 
-def blog_index(request):
+
+def blog_index (request):
     posts = Post.objects.all().order_by('-created_on')
     context = {
         "posts": posts,
     }
     return render(request, "blog_index.html", context)
 
-def blog_detail(request, pk):
+
+def blog_detail (request, pk):
     post = Post.objects.get(pk=pk)
-    context={
-        'post':post
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data['author'],
+                body=form.cleaned_data['body'],
+                post=post
+            )
+            comment.save()
+    comments = Comment.objects.filter(post=post)
+    context = {
+        'post': post,
+        'comments': comments,
+        'form': form
     }
     return render(request, 'blog_detail.html', context)
 
+
+def blog_category(request, category):
+    post = Post.objects.filter(
+        category__name__contains=categories
+    ).order_by('-created_on')
+    context = {
+        'post': post,
+        "category": category
+    }
+    return render(request, 'blog_category.html', context)
--- Drozdova_2023/project/views.py
+++ Drozdova_2023/project/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from .models import Project
+
+
 # Create your views here.
-def home_page(request):
-    return render(request, 'home_page.html')
+
 
 def project_index(request):
-    projects= Project.objects.all()
-    context={
-        'projects':projects
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
     }
     return render(request, 'project_index.html', context)
 
-def project_detail(request, pk):
+
+def project_details(request, pk):
     project = Project.objects.get(pk=pk)
-    context={
-        'project':project
+    context = {
+        'project': project
     }
     return render(request, 'project_detail.html', context)
-
